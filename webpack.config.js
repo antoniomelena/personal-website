36e70f499026658cd7df6ac7fa509c03c917f64e
Antonio Melena <melenaantonio@gmail.com>
2021-07-23 17:03:34 -0500

    created config file & learned how to compile sass with webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require("path");
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          "style-loader", //3. inject styles to DOM
+          "css-loader", //2. turns css into commonjs
+          "sass-loader", // 1. turn sass into css
+        ],
+      },
+    ],
+  },
+};
