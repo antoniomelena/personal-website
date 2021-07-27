00540ce0140451cd4194351cd447ded6bdc4d74d
Antonio Melena <melenaantonio@gmail.com>
2021-07-27 17:00:21 -0500

    fixed intersection observer, intersection observer is really cool

--- src/index.js
+++ src/index.js
@@ -1,23 +1,15 @@
 import "./scss/style.scss";
 
 const header = document.querySelector(".main-header");
-const sectionOne = document.querySelector(".container");
-
-// window.addEventListener("scroll", () => {
-//   const scrollPos = window.scrollY;
-//   if (scrollPos > 10) {
-//     header.classList.add("scrolled");
-//   } else {
-//     header.classList.remove("scrolled");
-//   }
-// });
+const sectionOne = document.querySelector(".hero");
 
 const sectionOneOptions = { rootMargin: "-200px 0px 0px 0px" };
 
 const sectionOneObserver = new IntersectionObserver(
   (entries, sectionOneObserver) => {
     entries.forEach((entry) => {
-      if (entry.isIntersecting) {
+      if (!entry.isIntersecting) {
+        console.log(entry.target);
         header.classList.add("scrolled");
       } else {
         header.classList.remove("scrolled");
@@ -28,3 +20,12 @@ const sectionOneObserver = new IntersectionObserver(
 );
 
 sectionOneObserver.observe(sectionOne);
+
+// window.addEventListener("scroll", () => {
+//   const scrollPos = window.scrollY;
+//   if (scrollPos > 10) {
+//     header.classList.add("scrolled");
+//   } else {
+//     header.classList.remove("scrolled");
+//   }
+// });
