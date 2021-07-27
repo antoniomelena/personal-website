64c72f1c6d1878f26f5737e8a53af28229cd4043
Antonio Melena <melenaantonio@gmail.com>
2021-07-27 15:28:12 -0500

    alot of css, added icons, navigation bar animations

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
-import "./style.scss";
+import "./scss/style.scss";
+
+const header = document.querySelector(".main-header");
+const sectionOne = document.querySelector(".container");
+
+// window.addEventListener("scroll", () => {
+//   const scrollPos = window.scrollY;
+//   if (scrollPos > 10) {
+//     header.classList.add("scrolled");
+//   } else {
+//     header.classList.remove("scrolled");
+//   }
+// });
+
+const sectionOneOptions = { rootMargin: "-200px 0px 0px 0px" };
+
+const sectionOneObserver = new IntersectionObserver(
+  (entries, sectionOneObserver) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
+  },
+  sectionOneOptions
+);
+
+sectionOneObserver.observe(sectionOne);
