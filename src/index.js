52685fcee9f0d841f59ebc5ed6a89813abcf3f46
Antonio Melena <melenaantonio@gmail.com>
2021-08-05 18:56:21 -0500

    reconfigured skills section

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import "./scss/style.scss";
 const header = document.querySelector(".main-header");
 const sectionOne = document.querySelector(".hero");
 
-const sectionOneOptions = { rootMargin: "-200px 0px 0px 0px" };
+const sectionOneOptions = { rootMargin: "-100px 0px 0px 0px" };
 
 const sectionOneObserver = new IntersectionObserver(
   (entries, sectionOneObserver) => {
     entries.forEach((entry) => {
       if (!entry.isIntersecting) {
-        console.log(entry.target);
         header.classList.add("scrolled");
       } else {
         header.classList.remove("scrolled");
@@ -20,12 +19,3 @@ const sectionOneObserver = new IntersectionObserver(
 );
 
 sectionOneObserver.observe(sectionOne);
-
-// window.addEventListener("scroll", () => {
-//   const scrollPos = window.scrollY;
-//   if (scrollPos > 10) {
-//     header.classList.add("scrolled");
-//   } else {
-//     header.classList.remove("scrolled");
-//   }
-// });
