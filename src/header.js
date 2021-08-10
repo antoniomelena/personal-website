7210de6b7a85a1a4bd2aaa93456b44511e073d77
Antonio Melena <melenaantonio@gmail.com>
2021-08-10 12:03:01 -0500

    moved header intersection observer to separate file

---
+++ src/header.js
@@ -0,0 +1,23 @@
+import "./scss/style.scss";
+import { changeSlide } from "./projects";
+
+const header = document.querySelector(".main-header");
+const sectionOne = document.querySelector(".hero");
+
+const sectionOneOptions = { rootMargin: "-100px 0px 0px 0px" };
+
+const sectionOneObserver = new IntersectionObserver(
+  (entries, sectionOneObserver) => {
+    entries.forEach((entry) => {
+      if (!entry.isIntersecting) {
+        header.classList.add("scrolled");
+      } else {
+        header.classList.remove("scrolled");
+      }
+    });
+  },
+  sectionOneOptions
+);
+
+sectionOneObserver.observe(sectionOne);
+export default sectionOneObserver;
